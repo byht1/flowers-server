f581c159c9b4d7188d1e48ecec6f4394ad412a22
byht1 <vitalikbyht1@gmail.com>
2022-11-22 16:32:50 +0200

    1

--- src/app.js
+++ src/app.js
@@ -18,9 +18,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//   res.json({ message: "start" });
-// });
+app.get("/", (req, res) => {
+  res.json({ message: "start" });
+});
 
 app.use("/reviews", reviewsRouts);
 app.use("/flowers", flowersRouts);
