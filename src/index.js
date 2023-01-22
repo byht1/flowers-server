2bddee1f6f9d4b6c89b234704e77c04f5b2731c2
Vitalik Dykhal <vitalikbyht1@gmail.com>
2023-01-22 19:56:27 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 const { DB_HOST, PORT = 3000 } = process.env;
+console.log("🚀  PORT", PORT);
 
 mongoose.set("strictQuery", true);
 
@@ -12,6 +13,7 @@ mongoose
     app.listen(PORT, () => console.log("Database connection successful"))
   )
   .catch((error) => {
+    console.log("ERROR");
     console.log(error);
     process.exit(1);
   });
